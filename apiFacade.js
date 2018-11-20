5c28fd902e786f0a1d5a62fe27aa577435dcdb1e
unknown <Andreasj54@gmail.com>
2018-11-20 10:58:28 +0100

    second commit

---
+++ apiFacade.js
@@ -0,0 +1,40 @@
+const URL = 'http://10.50.138.128:8084/jwtbackend';
+
+function handleHttpErrors(res) {
+    if (!res.ok) {
+        return Promise.reject({ status: res.status, fullError: res.json() });
+    }   
+    return res.json();
+}
+
+class ApiFacade {
+    fetchData = () => {
+        const options = this.makeOptions("GET", true); //True add's the token
+        return fetch(URL + "/api/info/user", options)
+            .then(handleHttpErrors);
+    };
+
+
+    fetchAllPersons = () => {
+        const options = this.makeOptions("GET");
+        return fetch(URL + "/api/swapi/person", options, true)
+            .then(handleHttpErrors)
+    }
+
+
+    makeOptions(method, body) {
+        var opts = {
+            method: method,
+            headers: {
+                "Content-type": "application/json",
+                Accept: "application/json"
+            }
+        };
+        if (body) {
+            opts.body = JSON.stringify(body);
+        }
+        return opts;
+    }
+}
+const facade = new ApiFacade();
+export default facade;
